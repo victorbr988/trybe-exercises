359dcd648e56ba48c5b450662693565c2dc2efcb
victor gabriel <gabrielvghs666@gmail.com>
2022-02-02 18:25:09 -0300

    adiciona a resposta para os exercicios de conteudo 02

--- Fundamentos/Bloco04/scripts-02.js
+++ Fundamentos/Bloco04/scripts-02.js
@@ -7,3 +7,12 @@ const patientInfo = {
 const patientEmail = 'ana@email.com';
 
 console.log(typeof patientId, typeof patientInfo, typeof isEnrolled, typeof patientEmail)
+
+// -------------------
+
+const base = 5;
+let height = 8;
+
+let area = base * height;
+let perimeter = (base * 2) + (height * 2)
+console.log(area, perimeter)
