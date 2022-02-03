e1d0c94439be2e88dd4772d92bc47ad64e811a03
victor gabriel <gabrielvghs666@gmail.com>
2022-02-03 11:59:29 -0300

    resposta do exercicio 05

---
+++ Fundamentos/Bloco04/Dia01/scripts-12.js
@@ -0,0 +1,13 @@
+const a = 60;
+const b = 60;
+const c = -10;
+
+if(a + b + c == 180){
+    console.log(true)
+}
+else if(a < 0 || b < 0 || c < 0){
+    console.log("valor inválido")
+}
+else{
+    console.log(false)
+}
