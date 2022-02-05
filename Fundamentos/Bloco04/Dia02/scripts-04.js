c94cd4a175e0195befd0a976672b3ac1585464f7
victor gabriel <gabrielvghs666@gmail.com>
2022-02-05 14:42:50 -0300

    adiciona resposta do exericio 04

---
+++ Fundamentos/Bloco04/Dia02/scripts-04.js
@@ -0,0 +1,19 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let soma = 0;
+
+for(let index = 0; index < numbers.length; index +=1){
+    soma += numbers[index];
+}
+
+let media = soma / numbers.length;
+
+if(media > 20){
+    console.log("valor maior que 20")
+}
+else if(media == 20){
+    console.log("o valor é igual a 20")
+}
+else{
+    console.log(media)
+}
