ed99bd85c4525607c93757901a55e8bea3596cc4
victor gabriel <gabrielvghs666@gmail.com>
2022-03-10 19:13:38 -0300

    adiciona refatoraçao para logica correta

--- Fundamentos/Bloco08/Dia01/exercise03.js
+++ Fundamentos/Bloco08/Dia01/exercise03.js
@@ -1,23 +1,21 @@
 function verifyGabaraito(correct, verify) {
   const responseCorect = correct;
   const reesponseVerify = verify;
+  let maxNota = responseCorect.length
   let nota = 0;
 
   for (let i = 0; i < responseCorect.length; i += 1) {
 
-    if(reesponseVerify.indexOf(responseCorect[i]) === -1) {
-      nota -= 0.5
-    }
-    if (reesponseVerify.indexOf(responseCorect[i]) > -1) {
-      nota += 1;
-      
+    if (reesponseVerify[i] === responseCorect[i]) {
+      nota += 1
     }
   }
   console.log(reesponseVerify);
+  console.log(responseCorect);
   return nota
 }
 
 function hof(correct, verify, callback) {
   return callback(correct, verify)
 }
-console.log(hof(['A', 'B', 'C', 'D'], ['A', 'B', 'A', 'D'], verifyGabaraito))
+console.log(hof(['E', 'A', 'E', 'E'], ['G', 'A', 'G', 'E'], verifyGabaraito))
