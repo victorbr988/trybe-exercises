f860900e02f1f68d79d91ca471f59625a1564f7b
victor gabriel <gabrielvghs666@gmail.com>
2022-03-11 17:03:36 -0300

    adiicona refatoraçao no codigo

--- Fundamentos/Bloco07/Dia 01/exercise4.js
+++ Fundamentos/Bloco07/Dia 01/exercise4.js
@@ -1,10 +1,14 @@
 // Crie uma função que receba uma frase e retorne qual a maior palavra.
 function sepear(phrase) {
   const splitPhrases = phrase.split(" ");
-  let maxPhrase = '';
-  for (let i = 0; i < splitPhrases.length; i += 1) {
-    maxPhrase = [splitPhrases[i]];
-    splitPhrases[i] > maxPhrase.length ? maxPhrase = splitPhrases[i] : maxPhrase = maxPhrase;
-  }
-  return maxPhrase;
+  
+  let maxWord= [];
+
+  splitPhrases.forEach((element, index) => {
+    maxWord[0] = (element);
+    element > splitPhrases[index] ? splitPhrases[index] = element: splitPhrases[index];
+  });
+
+  return maxWord;
 }
+console.log(sepear('hoje eu vou parar na gaiola'))
