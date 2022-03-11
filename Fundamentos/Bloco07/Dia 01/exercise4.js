7aed4ccecae888abd9d071b8859fc822540492ae
victor gabriel <gabrielvghs666@gmail.com>
2022-03-11 18:10:14 -0300

    adicinona correçao de logica

--- Fundamentos/Bloco07/Dia 01/exercise4.js
+++ Fundamentos/Bloco07/Dia 01/exercise4.js
@@ -1,14 +1,11 @@
 // Crie uma função que receba uma frase e retorne qual a maior palavra.
 function sepear(phrase) {
   const splitPhrases = phrase.split(" ");
-  
-  let maxWord= [];
+  let maxWord = '';
 
-  splitPhrases.forEach((element, index) => {
-    maxWord[0] = (element);
-    element > splitPhrases[index] ? splitPhrases[index] = element: splitPhrases[index];
+  splitPhrases.forEach((element) => {
+    element.length > maxWord.length ? maxWord = element: maxWord;
   });
 
   return maxWord;
 }
-console.log(sepear('hoje eu vou parar na gaiola'))
